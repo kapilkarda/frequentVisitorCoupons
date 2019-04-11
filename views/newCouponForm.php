13ea3a52e7d20e7b0e3028e52b45ea51887ee366
Sean Dez <sd@seandezoysa.com>
2019-04-11 23:55:44 +0700

    render hidden container on vanilla js

--- views/newCouponForm.php
+++ views/newCouponForm.php
@@ -19,7 +19,7 @@
       This new coupon should track a visitor's visits to...
     </label>
     </p>
-  
+    
     <p>
       <input
         type="radio"
@@ -32,47 +32,75 @@
         type="radio"
         name="trackingScope"
         value="specificPage"
+        id="specificPageRadio"
       >a specific product page
     </p>
   
-    <input
-      type="text"
-      name="specificPageField"
-      id="specificPageField"
-    >
-    
+    <div id="specificPageFieldContainer">
+      <label for="specificPageField">
+        Enter the target page you would like to track hits on
+      </label>
+      <input
+        type="text"
+        name="specificPageField"
+        id="specificPageField"
+      >
+    </div>
+
+
   </form>
   
-  <!-- include jquery -->
-  <script
-    src="https://code.jquery.com/jquery-3.4.0.slim.min.js"
-    integrity="sha256-ZaXnYkHGqIhqTbJ6MB4l9Frs/r7U4jlx7ir8PJYBqbI="
-    crossorigin="anonymous"></script>
-  
-  <script>
-    // conditional render the target box
-    $('input[name="trackingScope"]')
-      .click(function(event) {
-        if (event.target.value === 'specificPage') {
-          $("#specificPageField").show();
-        } else {
-          $("#specificPageField").hide();
-        }
-      });
-
-    $("#specificPageField").hide();
-    
-    
-    $('input[name="choose"]').click(function(e) {
-      if(e.target.value === 'yes') {
-        $('#optional').show();
-      } else {
-        $('#optional').hide();
-      }
-    });
-
-    $('#optional').hide();
-    
-  </script>
-  
 </div>
+
+
+
+<script>
+  const newCouponForm = document.querySelector("#newCouponForm");
+  const specificPageRadio = document.getElementById('specificPageRadio');
+  const specificPageFieldContainer = document.getElementById("specificPageFieldContainer");
+
+  newCouponForm.addEventListener('click',
+    function(event) {
+    if (event.target.value === 'specificPage') {
+      specificPageFieldContainer.style.display = "block";
+    } else if (event.target.value === 'wholeSite') {
+      specificPageFieldContainer.style.display = "none";
+    }
+  });
+  
+  
+  // function controlSpecificPageField() {
+  //   if (specificPageRadio.checked) {
+  //     console.log(specificPageRadio.checked, `=====specificPageRadio.checked=====`);
+  //     specificPageField.style.display = 'block'
+  //   } else {
+  //     specificPageField.style.display = 'none'
+  //   }
+  // }
+  
+  
+  // jQuery way
+  // conditional render the target box
+  // $('input[name="trackingScope"]')
+  //   .click(function(event) {
+  //     if (event.target.value === 'specificPage') {
+  //       $("#specificPageField").show();
+  //     } else {
+  //       $("#specificPageField").hide();
+  //     }
+  //   });
+  //
+  // $("#specificPageField").hide();
+  //
+  //
+  // $('input[name="choose"]').click(function(e) {
+  //   if(e.target.value === 'yes') {
+  //     $('#optional').show();
+  //   } else {
+  //     $('#optional').hide();
+  //   }
+  // });
+  //
+  // $('#optional').hide();
+
+</script>
