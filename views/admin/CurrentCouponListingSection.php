e32a719332d87b62e191f897b0834fd64181fee4
Sean Dez <sd@seandezoysa.com>
2019-05-10 18:24:34 +0700

    add table in view

--- views/admin/CurrentCouponListingSection.php
+++ views/admin/CurrentCouponListingSection.php
@@ -4,6 +4,7 @@
 <!-- Table Data -->
     <?php
       foreach ($targetData as $record):
+        
         if ($record->isSitewide === true) {
           $targetPage = 'Entire Website';
         } else {
@@ -11,46 +12,70 @@
         }
     ?>
       
-      <div class="couponListing">
-        <p class="label">
-          Coupon ID:
-        </p>
-        <p class="data">
-          <?php echo "{$record->targetId}" ?>
-        </p>
+      <table>
+        <thead>
+        <tr>
+          <td>Coupon ID</td>
+          <td>Target page</td>
+          <td>Total views</td>
+          <td>Pageviews before coupon displays</td>
+          <td>Number of times to show offer</td>
+          <td>Delete</td>
+        </tr>
+        </thead>
         
-        <p class="label">
-          Target display page:
-        </p>
-        <p class="data">
-          <?php echo "{$targetPage}" ?>
-        </p>
-        
-        <p class="label">
-          Total number of coupon views
-        </p>
-        <p class="data">
-          <?php echo "{$record->totalViews}" ?>
-        </p>
-        
-        <p class="label">
-          Number of views before a visitor sees coupon:
-        </p>
-        <p class="data">
-          <?php echo "{$record->displayThreshold}" ?>
-        </p>
-        
-        <p class="label">
-          Number of times offer is shown:
-        </p>
-        <p class="data">
-          <?php echo "{$record->offerCutoff}" ?>
-        </p>
-        
-        <button class="label deleteButton">
-          Delete this target (can not be undone)
-        </button>
-      </div>
+        <tbody id="couponTableBody">
+            <tr>
+              <td><?php echo "{$record->targetId}" ?></td>
+              <td><?php echo "{$targetPage}" ?></td>
+              <td><?php echo "{$record->totalViews}" ?></td>
+              <td><?php echo "{$record->displayThreshold}" ?></td>
+              <td><?php echo "{$record->offerCutoff}" ?></td>
+              <td>Delete</td>
+            </tr>
+        </tbody>
+      </table>
+      
+<!--      <div class="couponListing">-->
+<!--        <p class="label">-->
+<!--          Coupon ID:-->
+<!--        </p>-->
+<!--        <p class="data">-->
+<!--          --><?php //echo "{$record->targetId}" ?>
+<!--        </p>-->
+<!--        -->
+<!--        <p class="label">-->
+<!--          Target display page:-->
+<!--        </p>-->
+<!--        <p class="data">-->
+<!--          --><?php //echo "{$targetPage}" ?>
+<!--        </p>-->
+<!--        -->
+<!--        <p class="label">-->
+<!--          Total number of coupon views-->
+<!--        </p>-->
+<!--        <p class="data">-->
+<!--          --><?php //echo "{$record->totalViews}" ?>
+<!--        </p>-->
+<!--        -->
+<!--        <p class="label">-->
+<!--          Number of views before a visitor sees coupon:-->
+<!--        </p>-->
+<!--        <p class="data">-->
+<!--          --><?php //echo "{$record->displayThreshold}" ?>
+<!--        </p>-->
+<!--        -->
+<!--        <p class="label">-->
+<!--          Number of times offer is shown:-->
+<!--        </p>-->
+<!--        <p class="data">-->
+<!--          --><?php //echo "{$record->offerCutoff}" ?>
+<!--        </p>-->
+<!--        -->
+<!--        <button class="label deleteButton">-->
+<!--          Delete this target (can not be undone)-->
+<!--        </button>-->
+<!--      </div>-->
     <?php endforeach ?>
   
 <!--  Prev / Next buttons  -->
@@ -88,6 +113,10 @@
     }
     else return currentMarker;
   };
+  
+  const renderNewTableData = (jQuery, tableData) => {
+  
+  };
 
 
   // incrementSize should be negative for previous button
@@ -114,7 +143,7 @@
         setResultMarker(resultMarker, incrementSize)
         
         // re-render the table
-        // renderNewTableData(successResponse);
+        // renderNewTableData($, successResponse);
       },
       'json'
     );
@@ -124,10 +153,16 @@
   ///// JQUERY FUNCTIONS /////
   jQuery(document).ready(function($) {
     console.log(`====jquery loaded======`);
+    
     $('#previousButton').click(function() {
       console.log(`=====CLICK=====`);
-      $('.couponListing').html('');
-      ajaxLoadTableData($, 'next', );
+      $('#couponTableBody').html('');
+      ajaxLoadTableData($, -10);
+    });
+    $('#nextButton').click(function() {
+      console.log(`=====CLICK=====`);
+      $('#couponTableBody').html('');
+      ajaxLoadTableData($, 10);
     })
   });
   
